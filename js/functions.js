3c94759253e65fed96d472272b49798c4ad58329
iki_welt <iki.welt@ya.ru>
2024-02-25 23:12:31 +0300

    Дополнительное задание с созвона

--- js/functions.js
+++ js/functions.js
@@ -33,6 +33,34 @@ const getNumber = (string) => {
   return Number(numberString);
 };
 
+const addToString = (string = '', minLength = 1, adder = '') => {
+  if (string.length >= minLength) {
+    return string;
+  }
+
+  let addedString = '';
+  const adderLength = minLength - string.length;
+  const amountFullAdder = Math.floor(adderLength / adder.length);
+  let adderChars = adder;
+
+  if (adder.length > 0) {
+    adderChars = adder.split('');
+  }
+
+  for (let i = 0; i < (adderLength - adder.length * amountFullAdder); i++) {
+    addedString += adderChars[i];
+  }
+
+  while (addedString.length < adderLength) {
+    addedString += adder;
+  }
+
+  addedString += string;
+
+  return addedString;
+};
+
 checkStringLength();
 checkPalindrome('');
 getNumber();
+addToString();
