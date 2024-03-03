e225093cedfcb8bc9cc8745e922294160d28e86c
Keks <keks@htmlacademy.ru>
2024-03-03 10:34:26 +0300

    :heavy_check_mark: Сборка #3

---
+++ 3/js/functions.js
@@ -0,0 +1,66 @@
+const checkStringLength = (string = '', lenght = 1) => string.length <= lenght;
+
+const checkPalindrome = (string) => {
+  const normalizedString = string.replaceAll(' ', '').toLowerCase();
+  const reverseString = normalizedString.split('').reverse().join('');
+
+  return normalizedString === reverseString;
+};
+
+const getNumber = (string) => {
+  if (string === Math.round(string)) {
+    return Math.abs(string);
+  }
+
+  let numberString = '';
+  let normalizedString = string;
+
+  if (typeof string !== 'string') {
+    normalizedString = String(string);
+  }
+
+  for (let char = 0; char < normalizedString.length; char++) {
+
+    if (!(Number.isNaN(parseInt(normalizedString[char], 10)))) {
+      numberString += normalizedString[char];
+    }
+  }
+
+  if (numberString === '') {
+    numberString = NaN;
+  }
+
+  return Number(numberString);
+};
+
+const addToString = (string = '', minLength = 1, adder = '') => {
+  if (string.length >= minLength) {
+    return string;
+  }
+
+  let addedString = '';
+  const adderLength = minLength - string.length;
+  const amountFullAdder = Math.floor(adderLength / adder.length);
+  let adderChars = adder;
+
+  if (adder.length > 1) {
+    adderChars = adder.split('');
+  }
+
+  for (let i = 0; i < (adderLength - adder.length * amountFullAdder); i++) {
+    addedString += adderChars[i];
+  }
+
+  while (addedString.length < adderLength) {
+    addedString += adder;
+  }
+
+  addedString += string;
+
+  return addedString;
+};
+
+checkStringLength();
+checkPalindrome('');
+getNumber();
+addToString();
