f2d95ceaaa3facdc1c16d603969f807854e57649
iki_welt <iki.welt@ya.ru>
2024-03-04 20:13:27 +0300

    Актуализирует main.js

---
+++ js/main.js
@@ -0,0 +1,11 @@
+import {generateThread} from './generate-thread.js';
+import './filtering-thread.js';
+import './edit-image.js';
+import {checkStringLength, checkPalindrome, getNumber, addToString} from './functions.js';
+
+generateThread();
+
+checkStringLength();
+checkPalindrome();
+getNumber();
+addToString();
