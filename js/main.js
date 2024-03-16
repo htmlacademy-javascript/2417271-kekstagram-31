5d715f77f7c086cf5bca2b8fd354cb214fa2cff6
iki_welt <iki.welt@ya.ru>
2024-03-16 15:35:58 +0300

    актуализирует ветку

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 import {currentThread} from './generate-thread.js';
 import {addThumbnails} from './add-thumbnails.js';
-import './to-fullscreen-post.js';
-import './validate-form.js';
+import {thumbnailsContainerEl, onThumbnailClick} from './fullscreen-post-open-close.js';
 import './filtering-thread.js';
 import './edit-image.js';
 
 addThumbnails(currentThread);
+thumbnailsContainerEl.addEventListener('click', onThumbnailClick.bind(null, currentThread));
