33810990538c6f6aad6f1e65f6000c0f5b25cde5
iki_welt <iki.welt@ya.ru>
2024-03-20 13:14:12 +0300

    Чинит валидацию формы загрузки фото

--- js/validate-form.js
+++ js/validate-form.js
@@ -1,8 +1,9 @@
+const HASHTAG_LIMIT = 5;
+
 const uploadImgFormEl = document.querySelector('#upload-select-image');
 
 const editImgEl = uploadImgFormEl.querySelector('.img-upload__overlay');
 const hashtagsFieldEl = editImgEl.querySelector('#hashtags');
-const describtionFieldEl = editImgEl.querySelector('#description');
 
 const pristine = new Pristine(uploadImgFormEl, {
   classTo: 'img-upload__field-wrapper',
@@ -13,7 +14,7 @@ const pristine = new Pristine(uploadImgFormEl, {
 
 const isInLimitHashtags = (value) => {
   const hashtags = value.split(' ');
-  return hashtags.length <= 5;
+  return hashtags.length <= HASHTAG_LIMIT;
 };
 
 const isUndoubleHashtags = (value) => {
@@ -22,22 +23,17 @@ const isUndoubleHashtags = (value) => {
 };
 
 const isValidHashtags = (value) => {
-  const hashtagPattern = /^#[a-zа-яё0-9]{1,19}$/gi;
-  const hashtags = value.split(' ');
-  // работает некорректно: при введении второго хэштэга продолжает считать его ошибочным, даже когда после # введены валидные символы
-  // const invalidEl = hashtags.find((hashtag) => !hashtagPattern.test(hashtag));
-  // if (!invalidEl || value === '') {
-  //   return true;
-  // }
-  // return false;
-  // та же проблема
-  // return hashtags.every((hashtag) => hashtagPattern.test(hashtag)) || value === '';
+  if (value === '') {
+    return true;
+  }
+  const hashtagPattern = /^#[a-zа-яё0-9]{1,19}$/i;
+  const hashtags = value.trim().split(' ');
+  return hashtags.every((hashtag) => hashtagPattern.test(hashtag));
 };
 
 pristine.addValidator(hashtagsFieldEl, isInLimitHashtags, 'Превышено количество хэштегов');
 pristine.addValidator(hashtagsFieldEl, isUndoubleHashtags, 'Хэштеги повторяются');
 pristine.addValidator(hashtagsFieldEl, isValidHashtags, 'Введён невалидный хэштег');
-pristine.addValidator(describtionFieldEl);
 
 uploadImgFormEl.addEventListener('submit', (evt) => {
   evt.preventDefault();
